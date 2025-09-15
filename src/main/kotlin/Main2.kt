be10268cf25caca835bb484fe8de38c4befa736b
user1 <raden7george@gmail.com>
2025-09-15 14:11:56 +0700

    pert2

---
+++ src/main/kotlin/Main2.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Hello")
+    println(1)
+    println(true)
+    println(2.0)
+    println(2 * 5 / 3)
+    println(5 % 10 + 5)
+    println(5.10 % 27.5 + 5)
+    println(25.0 % 100)
+    println("%.0f".format(28.08 % 99.01))
+    println("%.5f".format(27.91 % 5.01))
+    println("%.4f".format(21.08 % 7.01))
+
+    val text: String = "Hello"
+    print(text)
+}
