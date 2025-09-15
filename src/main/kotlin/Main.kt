d5620821e242ef63a965dd224587d3fc50240642
user1 <raden7george@gmail.com>
2025-09-15 11:17:57 +0700

    first learn

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    print("Hello world\n")
+//    Ini komentar
+    println("Hello World")
+
+    /*Contoh Multi comment */
+    println('A') // Mencetak hanya satu karakter saja
+//    NPM, Nama, Program Studi
+    println("NPM \t\t\t: 2125250066")
+    println("Nama \t\t\t: R George Samuel Bud")
+    println("Program Studi \t: Informatika")
+    println("Quote \t\t\t: \"Wakanda Forever\"")
+    println("Nickname \t\t: \"Zaworld\"")
+
+//    Format Code Ctrl + Alt + L
+
+}
